95957c8fe2e38aa4a8006013224712ac46475e02
Jasmine Persson <jasmine.persson@hyperisland.se>
2020-09-30 10:59:47 +0200

    First commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Restaurant booking system with React and Node JS</h1>
+    </div>
+  );
+}
+
+export default App;

2396cd04100a9f7ff628e38e500f8726fee67c4e
Jasmine Persson <jasmine.persson@hyperisland.se>
2020-09-30 23:13:43 +0200

    Cleaned code

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -13,19 +13,3 @@ function Navigation() {
 }
 
 export default Navigation;
-
-
-// export default props => {
-//     return (
-//         <div>
-//             <Navbar color="light" light expand="md">
-//                 <NavbarBrand className="navbar-header" onClick={_ => {
-//                     props.setPage(0);
-//                 }}>
-//                     Restaurant booking
-//             </NavbarBrand>
-//             </Navbar>
-//         </div>
-//     )
-
-// }

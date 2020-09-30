eb8ef06a9a59994df962fcc37b5d4e21000c4925
Hugo Johansson <hugo.johansson@hyperisland.se>
2020-09-30 11:44:06 +0200

    Added text

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>Restaurant booking system with React and Node JS</h1>
+      <h1>
+        Restaurant booking system with React and Node JS, this is backend!
+      </h1>
     </div>
   );
 }

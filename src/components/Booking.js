040d195b7143b8c693095fa60e6c7674e2e389dc
Hugo Johansson <hugo.johansson@hyperisland.se>
2020-10-07 15:33:33 +0200

    added some functions in the booking

--- src/components/Booking.js
+++ src/components/Booking.js
@@ -2,11 +2,43 @@ import React from "react";
 import { row, col, button } from "reactstrap";
 
 const Booking = () => {
+  const handleFirstName = (e) => {
+    console.log(e.target.value);
+  };
+
+  const handleLastName = (e) => {
+    console.log(e.target.value);
+  };
+
+  const handleEmail = (e) => {
+    console.log(e.target.value);
+  };
+
+  const handlePhone = (e) => {
+    console.log(e.target.value);
+  };
+  const handleDate = (e) => {
+    console.log(e.target.value);
+  };
+
+  const handlePeople = (e) => {
+    console.log(e.target.value);
+  };
+
+  const handTime = (e) => {
+    console.log(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // Send info to db to save the booking
+  };
   return (
     <div className="text-center align-items-center m-5">
       <h1>Make a reservation</h1>
       <div className="container-booking">
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="form-row">
             <div className="form-group col-md-6">
               <label for="firstName">Name</label>
@@ -14,6 +46,7 @@ const Booking = () => {
                 type="text"
                 className="form-control"
                 id="firstName"
+                onChange={handleFirstName}
                 placeholder="ex. Ann"
               />
             </div>
@@ -22,6 +55,7 @@ const Booking = () => {
               <input
                 type="text"
                 className="form-control"
+                onChange={handleLastName}
                 id="lastName"
                 placeholder="ex. Jonsson"
               />
@@ -34,6 +68,7 @@ const Booking = () => {
                 type="text"
                 className="form-control"
                 id="email"
+                onChange={handleEmail}
                 placeholder="ex. ann.jonsson@hotmail.com"
               />
             </div>
@@ -43,6 +78,7 @@ const Booking = () => {
                 type="text"
                 className="form-control"
                 id="phoneNumber"
+                onChange={handlePhone}
                 placeholder="ex. 0701236986"
               />
             </div>
@@ -50,11 +86,16 @@ const Booking = () => {
           <div className="form-row">
             <div className="form-group col-md-6">
               <label for="date">Date</label>
-              <input type="date" className="form-control" id="date" />
+              <input
+                type="date"
+                className="form-control"
+                id="date"
+                onChange={handleDate}
+              />
             </div>
             <div className="form-group col-md-4">
               <label for="time">Time</label>
-              <select id="time" className="form-control">
+              <select id="time" className="form-control" onChange={handTime}>
                 <option selected>Choose time</option>
                 <option>18:00</option>
                 <option>21:00</option>
@@ -62,14 +103,18 @@ const Booking = () => {
             </div>
             <div className="form-group col-md-2">
               <label for="sizeparty">Party size</label>
-              <input
-                type="number"
+              <select
+                id="time"
                 className="form-control"
-                id="sizeParty"
-                min="1"
-                max="6"
-                placeholder="1 - 6 people"
-              />
+                onChange={handlePeople}
+              >
+                <option>1</option>
+                <option>2</option>
+                <option>3</option>
+                <option>4</option>
+                <option>5</option>
+                <option>6</option>
+              </select>
             </div>
           </div>
           <button type="submit" className="btn btn-primary col-md-12">

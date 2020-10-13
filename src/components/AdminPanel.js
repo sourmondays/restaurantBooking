64006732be42ba6f65a725ce71d2a54e9771e292
Jasmine Persson <jasmine.persson@hyperisland.se>
2020-10-13 13:39:24 +0200

    Changed buttons admin nav

--- src/components/AdminPanel.js
+++ src/components/AdminPanel.js
@@ -1,19 +1,22 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { Button, Space } from 'antd';
 
 const AdminPanel = () => {
     return (
         <div>
             <div className="text-right align-items-right mt-5 mr-5">
-                <Link to="/adminbookings">
-                    <button type="button" className="btn btn-dark mr-2">Bookings</button>
-                </Link>
-                <Link to="/admincreate">
-                    <button type="button" className="btn btn-dark mr-2">Create</button>
-                </Link>
-                <Link to="/adminsettings">
-                    <button type="button" className="btn btn-dark">Settings</button>
-                </Link>
+                <Space direction="horizontal">
+                    <Link to="/adminbookings">
+                        <Button className="my-button" type="primary">Bookings</Button>
+                    </Link>
+                    <Link to="/admincreate">
+                        <Button className="my-button" type="primary">Create</Button>
+                    </Link>
+                    <Link to="/adminsettings">
+                        <Button className="my-button" type="primary">Settings</Button>
+                    </Link>
+                </Space>
             </div>
         </div>
     );

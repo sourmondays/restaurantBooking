6184d0fea5ca7f57fc94c1b597b4c907dfc126b6
Jasmine Persson <jasmine.persson@hyperisland.se>
2020-10-13 13:22:39 +0200

    Created forms on admin panel

--- src/components/AdminPanel.js
+++ src/components/AdminPanel.js
@@ -1,23 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Button, Space } from 'antd';
-
 
 const AdminPanel = () => {
     return (
         <div>
             <div className="text-right align-items-right mt-5 mr-5">
-                <Space direction="horizontal">
-                    <Link to="/adminbookings">
-                        <Button type="primary">Bookings</Button>
-                    </Link>
-                    <Link to="/admincreate">
-                        <Button type="primary">Create</Button>
-                    </Link>
-                    <Link to="/adminsettings">
-                        <Button type="primary">Settings</Button>
-                    </Link>
-                </Space>
+                <Link to="/adminbookings">
+                    <button type="button" className="btn btn-dark mr-2">Bookings</button>
+                </Link>
+                <Link to="/admincreate">
+                    <button type="button" className="btn btn-dark mr-2">Create</button>
+                </Link>
+                <Link to="/adminsettings">
+                    <button type="button" className="btn btn-dark">Settings</button>
+                </Link>
             </div>
         </div>
     );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ function Navigation() {
     return (
         <div>
 
-            <nav className="navbar navbar-dark bg-dark">
+            <nav className="navbar navbar-light bg-light">
                 <a className="navbar-brand" href="/">Restaurant booking</a>
             </nav>
 

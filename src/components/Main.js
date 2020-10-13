2d3f3d099bd349c3cecf754b73f5710be7fde6ac
Jasmine Persson <jasmine.persson@hyperisland.se>
2020-10-13 13:46:13 +0200

    Changes on main page

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,31 +1,22 @@
 import React from "react";
-import { Row, Col, Button } from "reactstrap";
 import { Link } from "react-router-dom";
+import { Button } from 'antd';
 
 const Main = (props) => {
   return (
     <div>
-      <Row noGutters className="text-center align-items-center mt-5">
-        <Col>
-          <h4 className="restaurant-header">
-            Make your reservation for a fancy dinner!
+      <div className="text-center align-items-center mt-5">
+
+        <h4 className="restaurant-header">
+          Make your reservation for a fancy dinner!
           </h4>
-        </Col>
-      </Row>
-      <Row noGutters className="justify-content-center">
+      </div>
+
+      <div className="text-center align-items-center mt-2">
         <Link to="/booking">
-          <Button color="none" className="make-reservation-btn btn btn-dark">
-            Make a reservation
-          </Button>
+          <Button type="primary">Make a reservation</Button>
         </Link>
-        {/*  ta bort denna sen */}
-        <Link to="/adminpanel">
-          <Button color="none" className="make-reservation-btn btn btn-dark">
-            Login as Admin
-          </Button>
-        </Link>
-        {/*  ta bort denna sen */}
-      </Row>
+      </div>
     </div>
   );
 };
